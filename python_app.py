746cfd37052e58b00dc49330e2f6efd990a32768
guavalines <konstrictor@gmail.com>
2023-04-29 20:48:59 -0700

    initial commit

---
+++ python_app.py
@@ -0,0 +1,5 @@
+# define the functions needed: add, sub, mul, div
+# print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user wants to exit
