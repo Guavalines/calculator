8f7e322cf19fe9b21c9df02e34e8de93b55d5e68
guavalines <konstrictor@gmail.com>
2023-04-29 21:04:48 -0700

    defined functions add, sub, mul, div

--- python_app.py
+++ python_app.py
@@ -3,3 +3,20 @@
 # ask for values
 # call the functions
 # while loop to continue the program until the user wants to exit
+
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer))
+
+def div(a, b):
+    answer = a / b
+    print(str(a) + " / " + str(b) + " = " + str(answer))
